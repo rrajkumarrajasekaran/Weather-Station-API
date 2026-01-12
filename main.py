9fdba283396f5b3e296b5cc707d8db29fc57d427
rajkumar <rrajkumarr104@gmail.com>
2026-01-12 20:54:48 +0530

    Commit first check

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
 @app.route("/api/v1/<station>/<date>")
-def rest_implement(station,date):
-    temperature = 29
-    return {"station":station,
-            "date":date,
-            "temperature":temperature}
+def about(station, date):
+    temperature = 24
+    return {"Station":station, "Date":date, "Temperature":temperature}
 
 if __name__ == "__main__":
     app.run(debug=True)
