9c50e29643416aa2d9a53293f1235c7df0d4742d
rajkumar <rrajkumarr104@gmail.com>
2026-01-18 18:08:13 +0530

    - Created endpoint to read lifetime temperature value of a particular station -Created endpoint to read temperature value of a station over a given year

--- main.py
+++ main.py
@@ -3,16 +3,34 @@ import pandas as pd
 
 app = Flask(__name__)
 
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
+def temperature_station_date(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = (df.loc[df["    DATE"] == date]["   TG"].squeeze()/10).item()
     return {"Station":station, "Date":date, "Temperature":temperature}
+
+@app.route("/api/v1/<station>")
+def temperature_station_all(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    results = df.to_dict(orient="records")
+    return results
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def temperature_station_year(station,year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    results = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return results
 
 if __name__ == "__main__":
     app.run(debug=True, port=6234)
